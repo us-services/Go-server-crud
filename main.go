5e2b65da9c7c84ec48b0818d3714b70c53b6b7d7
Utkarsh Sinha <utkarshsinha@Utkarshs-MacBook-Air.local>
2025-03-23 00:02:49 +0530

    gte and post calls + tests

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+type Item struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var (
+	items  []Item
+	mutex  sync.Mutex
+	nextID int = 1
+)
+
+func getItems(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	mutex.Lock()
+	defer mutex.Unlock()
+	json.NewEncoder(w).Encode(items)
+}
+
+func addItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newItem Item
+	if err := json.NewDecoder(r.Body).Decode(&newItem); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+	newItem.ID = nextID
+	nextID++
+	items = append(items, newItem)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newItem)
+}
+
+func main() {
+	http.HandleFunc("/items", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			getItems(w)
+		case http.MethodPost:
+			addItem(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	fmt.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
